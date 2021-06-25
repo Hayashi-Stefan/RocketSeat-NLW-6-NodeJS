7b2f45ea0974e7c5790379655255c13abc6a682a
Hayashi-Stefan <stefanken1996@hotmail.com>
2021-06-25 20:43:18 -0300

    Server - Tratativa de erros c/ middleware

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors"
+
 import { router } from "./routes"
 import "./database";
 
@@ -7,6 +9,20 @@ const app = express();
 
 app.use(express.json())
 app.use(router);
+
+/* Middleware */
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
+})
 
 app.listen(8083, () => {
   console.log(`Server is running at port: 3000`);
